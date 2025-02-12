d4e410d38977dd3c4bbcf7fc02d66300028fc9bf
Peace <devbypeace@gmail.com>
2025-02-12 11:48:51 +0100

    darkMode

--- script.js
+++ script.js
@@ -1,5 +1,10 @@
-// ELements
+/////////// ELements
 const sound = document.querySelector(".audio");
+const lightBulb = document.getElementById("btnlight");
+const body = document.querySelector("body");
+const header = document.querySelector("header");
+
+//////////// FUNCTIONS
 
 //   Audio
 const newSound = function () {
@@ -7,7 +12,29 @@ const newSound = function () {
   audio.play();
 };
 
+// Bg color
+const changebg = function () {
+  let currentColor = getComputedStyle(body).backgroundColor;
+  if (currentColor === "rgb(0, 0, 0)") {
+    body.style.backgroundColor = "white";
+    header.style.color = "black";
+    lightBulb.style.color = "black";
+    sound.style.backgroundColor = "black";
+    sound.style.color = "white";
 
+  } else {
+    body.style.backgroundColor = "black";
+    header.style.color = "white";
+    lightBulb.style.color = "white";
+    sound.style.backgroundColor = "white";
+    sound.style.color = "black";
+  }
+};
 
-//  Buttons
+////////////  BUTTONS
+
+// sound
 sound.addEventListener("click", newSound);
+
+// switch
+lightBulb.addEventListener("click", changebg);
